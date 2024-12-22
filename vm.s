ea6b9e8ca4f888079224c41c4189f78c741c436b
jarman <jarukitman@yahoo.com>
2024-12-22 22:57:20 +0700

    modified:   vm 	modified:   vm.s

--- vm.s
+++ vm.s
@@ -22,13 +22,23 @@ push:
 	sub	rsp, 32
 	mov	DWORD PTR -20[rbp], edi
 	mov	DWORD PTR -24[rbp], esi
+	mov	DWORD PTR -28[rbp], edx
 	mov	edi, 16
 	call	malloc@PLT
 	mov	QWORD PTR -8[rbp], rax
-	mov	eax, DWORD PTR -24[rbp]
-	movsx	rdx, eax
+	mov	edi, 8
+	call	malloc@PLT
+	mov	rdx, rax
 	mov	rax, QWORD PTR -8[rbp]
 	mov	QWORD PTR [rax], rdx
+	mov	rax, QWORD PTR -8[rbp]
+	mov	rax, QWORD PTR [rax]
+	mov	edx, DWORD PTR -24[rbp]
+	mov	DWORD PTR [rax], edx
+	mov	rax, QWORD PTR -8[rbp]
+	mov	rax, QWORD PTR [rax]
+	mov	edx, DWORD PTR -28[rbp]
+	mov	DWORD PTR 4[rax], edx
 	mov	eax, DWORD PTR -20[rbp]
 	cdqe
 	lea	rdx, 0[0+rax*8]
@@ -97,7 +107,7 @@ pop:
 	lea	rax, vars[rip]
 	mov	rax, QWORD PTR [rdx+rax]
 	mov	rax, QWORD PTR 8[rax]
-	mov	QWORD PTR -8[rbp], rax
+	mov	QWORD PTR -16[rbp], rax
 	mov	eax, DWORD PTR -20[rbp]
 	cdqe
 	lea	rdx, 0[0+rax*8]
@@ -119,13 +129,22 @@ pop:
 	mov	edx, DWORD PTR [rax]
 	sub	edx, 1
 	mov	DWORD PTR [rax], edx
-	mov	rax, QWORD PTR -8[rbp]
+	mov	rax, QWORD PTR -16[rbp]
 	mov	rax, QWORD PTR [rax]
-	mov	DWORD PTR -12[rbp], eax
-	mov	rax, QWORD PTR -8[rbp]
+	mov	eax, DWORD PTR [rax]
+	mov	DWORD PTR -8[rbp], eax
+	mov	rax, QWORD PTR -16[rbp]
+	mov	rax, QWORD PTR [rax]
+	mov	eax, DWORD PTR 4[rax]
+	mov	DWORD PTR -4[rbp], eax
+	mov	rax, QWORD PTR -16[rbp]
+	mov	rax, QWORD PTR [rax]
 	mov	rdi, rax
 	call	free@PLT
-	mov	eax, DWORD PTR -12[rbp]
+	mov	rax, QWORD PTR -16[rbp]
+	mov	rdi, rax
+	call	free@PLT
+	mov	rax, QWORD PTR -8[rbp]
 	leave
 	.cfi_def_cfa 7, 8
 	ret
@@ -142,14 +161,26 @@ peek:
 	.cfi_offset 6, -16
 	mov	rbp, rsp
 	.cfi_def_cfa_register 6
-	mov	DWORD PTR -4[rbp], edi
-	mov	eax, DWORD PTR -4[rbp]
+	mov	DWORD PTR -20[rbp], edi
+	mov	eax, DWORD PTR -20[rbp]
 	cdqe
 	lea	rdx, 0[0+rax*8]
 	lea	rax, vars[rip]
 	mov	rax, QWORD PTR [rdx+rax]
 	mov	rax, QWORD PTR 8[rax]
 	mov	rax, QWORD PTR [rax]
+	mov	eax, DWORD PTR [rax]
+	mov	DWORD PTR -8[rbp], eax
+	mov	eax, DWORD PTR -20[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*8]
+	lea	rax, vars[rip]
+	mov	rax, QWORD PTR [rdx+rax]
+	mov	rax, QWORD PTR 8[rax]
+	mov	rax, QWORD PTR [rax]
+	mov	eax, DWORD PTR 4[rax]
+	mov	DWORD PTR -4[rbp], eax
+	mov	rax, QWORD PTR -8[rbp]
 	pop	rbp
 	.cfi_def_cfa 7, 8
 	ret
@@ -210,6 +241,10 @@ flush:
 	mov	rax, QWORD PTR 8[rax]
 	mov	QWORD PTR -8[rbp], rax
 	mov	rax, QWORD PTR -16[rbp]
+	mov	rax, QWORD PTR [rax]
+	mov	rdi, rax
+	call	free@PLT
+	mov	rax, QWORD PTR -16[rbp]
 	mov	rdi, rax
 	call	free@PLT
 	mov	rax, QWORD PTR -8[rbp]
@@ -269,6 +304,10 @@ ren:
 	mov	rax, QWORD PTR 8[rax]
 	mov	QWORD PTR -16[rbp], rax
 	mov	rax, QWORD PTR -24[rbp]
+	mov	rax, QWORD PTR [rax]
+	mov	rdi, rax
+	call	free@PLT
+	mov	rax, QWORD PTR -24[rbp]
 	mov	rdi, rax
 	call	free@PLT
 	mov	rax, QWORD PTR -16[rbp]
@@ -307,9 +346,9 @@ ren:
 	.cfi_endproc
 .LFE11:
 	.size	ren, .-ren
-	.globl	main
-	.type	main, @function
-main:
+	.globl	clone
+	.type	clone, @function
+clone:
 .LFB12:
 	.cfi_startproc
 	push	rbp
@@ -317,281 +356,537 @@ main:
 	.cfi_offset 6, -16
 	mov	rbp, rsp
 	.cfi_def_cfa_register 6
-	push	rbx
-	sub	rsp, 24
-	.cfi_offset 3, -24
-	mov	DWORD PTR -20[rbp], 0
+	sub	rsp, 48
+	mov	DWORD PTR -36[rbp], edi
+	mov	DWORD PTR -40[rbp], esi
+	mov	eax, DWORD PTR -36[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*8]
+	lea	rax, vars[rip]
+	mov	rax, QWORD PTR [rdx+rax]
+	mov	rax, QWORD PTR 8[rax]
+	mov	QWORD PTR -24[rbp], rax
+	mov	eax, DWORD PTR -40[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*8]
+	lea	rax, vars[rip]
+	mov	rax, QWORD PTR [rdx+rax]
+	mov	rax, QWORD PTR 8[rax]
+	mov	QWORD PTR -16[rbp], rax
+	mov	eax, DWORD PTR -36[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*8]
+	lea	rax, vars[rip]
+	mov	rdx, QWORD PTR [rdx+rax]
+	mov	eax, DWORD PTR -40[rbp]
+	cdqe
+	lea	rcx, 0[0+rax*8]
+	lea	rax, vars[rip]
+	mov	rax, QWORD PTR [rcx+rax]
+	mov	edx, DWORD PTR [rdx]
+	mov	DWORD PTR [rax], edx
 	jmp	.L17
 .L18:
+	mov	rax, QWORD PTR -16[rbp]
+	mov	rax, QWORD PTR 8[rax]
+	mov	QWORD PTR -8[rbp], rax
+	mov	rax, QWORD PTR -16[rbp]
+	mov	rax, QWORD PTR [rax]
+	mov	rdi, rax
+	call	free@PLT
+	mov	rax, QWORD PTR -16[rbp]
+	mov	rdi, rax
+	call	free@PLT
+	mov	rax, QWORD PTR -8[rbp]
+	mov	QWORD PTR -16[rbp], rax
+.L17:
+	cmp	QWORD PTR -16[rbp], 0
+	jne	.L18
 	mov	edi, 16
 	call	malloc@PLT
-	mov	rcx, rax
-	mov	eax, DWORD PTR -20[rbp]
-	cdqe
-	lea	rdx, 0[0+rax*8]
-	lea	rax, vars[rip]
-	mov	QWORD PTR [rdx+rax], rcx
-	mov	eax, DWORD PTR -20[rbp]
+	mov	QWORD PTR -16[rbp], rax
+	mov	eax, DWORD PTR -40[rbp]
 	cdqe
 	lea	rdx, 0[0+rax*8]
 	lea	rax, vars[rip]
 	mov	rax, QWORD PTR [rdx+rax]
-	mov	DWORD PTR [rax], 0
-	mov	eax, DWORD PTR -20[rbp]
-	cdqe
-	lea	rdx, 0[0+rax*8]
-	lea	rax, vars[rip]
-	mov	rax, QWORD PTR [rdx+rax]
-	mov	QWORD PTR 8[rax], 0
-	add	DWORD PTR -20[rbp], 1
-.L17:
-	cmp	DWORD PTR -20[rbp], 2
-	jle	.L18
-	mov	esi, 0
-	mov	edi, 0
-	call	push
+	mov	rdx, QWORD PTR -16[rbp]
+	mov	QWORD PTR 8[rax], rdx
 	jmp	.L19
 .L20:
-	mov	edi, 0
-	call	size
-	test	eax, eax
-	jne	.L21
-	mov	eax, 1
-	jmp	.L22
-.L21:
-	mov	edi, 0
-	call	pop
-	cmp	eax, 6
-	ja	.L23
-	mov	eax, eax
-	lea	rdx, 0[0+rax*4]
-	lea	rax, .L25[rip]
-	mov	eax, DWORD PTR [rdx+rax]
-	cdqe
-	lea	rdx, .L25[rip]
-	add	rax, rdx
-	jmp	rax
-	.section	.rodata
-	.align 4
-	.align 4
-.L25:
-	.long	.L31-.L25
-	.long	.L39-.L25
-	.long	.L40-.L25
-	.long	.L41-.L25
-	.long	.L42-.L25
-	.long	.L19-.L25
-	.long	.L38-.L25
-	.text
-.L31:
-	mov	eax, 0
-	jmp	.L22
-.L23:
-	mov	eax, 1
-	jmp	.L22
-.L39:
-	nop
-.L32:
-	mov	edi, 0
-	call	pop
-	mov	ebx, eax
-	mov	edi, 0
-	call	pop
-	mov	esi, ebx
-	mov	edi, eax
-	call	push
-	jmp	.L20
-.L40:
-	nop
-.L33:
-	mov	edi, 0
-	call	pop
-	mov	ebx, eax
-	mov	edi, 0
-	call	pop
-	mov	esi, ebx
-	mov	edi, eax
-	call	ren
-	jmp	.L20
-.L41:
-	nop
-.L34:
-	mov	edi, 1
-	call	pop
-	mov	edi, eax
-	call	putchar@PLT
-	mov	edi, 1
-	call	size
-	test	eax, eax
-	jle	.L20
-	mov	esi, 3
-	mov	edi, 0
-	call	push
-	jmp	.L20
-.L42:
-	nop
-.L35:
-	mov	esi, 2
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 2
-	mov	edi, 0
-	call	push
-	jmp	.L20
+	mov	edi, 8
+	call	malloc@PLT
+	mov	rdx, rax
+	mov	rax, QWORD PTR -16[rbp]
+	mov	QWORD PTR [rax], rdx
+	mov	rax, QWORD PTR -24[rbp]
+	mov	rdx, QWORD PTR [rax]
+	mov	rax, QWORD PTR -16[rbp]
+	mov	rax, QWORD PTR [rax]
+	mov	edx, DWORD PTR [rdx]
+	mov	DWORD PTR [rax], edx
+	mov	rax, QWORD PTR -24[rbp]
+	mov	rdx, QWORD PTR [rax]
+	mov	rax, QWORD PTR -16[rbp]
+	mov	rax, QWORD PTR [rax]
+	mov	edx, DWORD PTR 4[rdx]
+	mov	DWORD PTR 4[rax], edx
+	mov	rax, QWORD PTR -24[rbp]
+	mov	rax, QWORD PTR 8[rax]
+	mov	QWORD PTR -24[rbp], rax
+	mov	rax, QWORD PTR -16[rbp]
+	mov	rax, QWORD PTR 8[rax]
+	mov	QWORD PTR -16[rbp], rax
+	mov	edi, 16
+	call	malloc@PLT
+	mov	QWORD PTR -16[rbp], rax
 .L19:
-	mov	esi, 3
-	mov	edi, 0
-	call	push
-	mov	esi, 6
-	mov	edi, 0
-	call	push
-	mov	esi, 3
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 72
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 101
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 108
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 108
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 111
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 44
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 32
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	jmp	.L20
-.L38:
+	cmp	QWORD PTR -24[rbp], 0
+	jne	.L20
+	mov	rax, QWORD PTR -16[rbp]
+	mov	rdi, rax
+	call	free@PLT
+	mov	QWORD PTR -16[rbp], 0
 	nop
-.L36:
-	mov	esi, 4
-	mov	edi, 0
-	call	push
-	mov	esi, 2
-	mov	edi, 0
-	call	push
-	mov	esi, 87
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 2
-	mov	edi, 0
-	call	push
-	mov	esi, 111
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 2
-	mov	edi, 0
-	call	push
-	mov	esi, 114
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 2
-	mov	edi, 0
-	call	push
-	mov	esi, 108
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 2
-	mov	edi, 0
-	call	push
-	mov	esi, 100
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 2
-	mov	edi, 0
-	call	push
-	mov	esi, 33
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	mov	esi, 2
-	mov	edi, 0
-	call	push
-	mov	esi, 10
-	mov	edi, 0
-	call	push
-	mov	esi, 1
-	mov	edi, 0
-	call	push
-	jmp	.L20
-.L22:
-	mov	rbx, QWORD PTR -8[rbp]
 	leave
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE12:
+	.size	clone, .-clone
+	.section	.rodata
+.LC2:
+	.string	"%d"
+.LC3:
+	.string	"%f"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB13:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	push	r12
+	push	rbx
+	sub	rsp, 48
+	.cfi_offset 12, -24
+	.cfi_offset 3, -32
+	mov	rax, QWORD PTR fs:40
+	mov	QWORD PTR -24[rbp], rax
+	xor	eax, eax
+	mov	DWORD PTR -36[rbp], 0
+	jmp	.L22
+.L23:
+	mov	edi, 16
+	call	malloc@PLT
+	mov	rcx, rax
+	mov	eax, DWORD PTR -36[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*8]
+	lea	rax, vars[rip]
+	mov	QWORD PTR [rdx+rax], rcx
+	mov	eax, DWORD PTR -36[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*8]
+	lea	rax, vars[rip]
+	mov	rax, QWORD PTR [rdx+rax]
+	mov	DWORD PTR [rax], 0
+	mov	eax, DWORD PTR -36[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*8]
+	lea	rax, vars[rip]
+	mov	rax, QWORD PTR [rdx+rax]
+	mov	QWORD PTR 8[rax], 0
+	add	DWORD PTR -36[rbp], 1
+.L22:
+	cmp	DWORD PTR -36[rbp], 2
+	jle	.L23
+	mov	DWORD PTR -32[rbp], 0
+	mov	DWORD PTR -28[rbp], 0
+	mov	edx, 0
+	mov	esi, 0
+	mov	edi, 0
+	call	push
+	mov	edx, 0
+	mov	esi, 11
+	mov	edi, 0
+	call	push
+	mov	edx, 0
+	mov	esi, 11
+	mov	edi, 0
+	call	push
+	mov	edx, 3
+	mov	esi, 10
+	mov	edi, 0
+	call	push
+	mov	edx, 0
+	mov	esi, 10
+	mov	edi, 0
+	call	push
+	mov	edx, 1
+	mov	esi, 3
+	mov	edi, 0
+	call	push
+	mov	edx, 0
+	mov	esi, 2
+	mov	edi, 0
+	call	push
+	mov	edx, -1
+	mov	esi, 2
+	mov	edi, 0
+	call	push
+	mov	edx, 0
+	mov	esi, 11
+	mov	edi, 0
+	call	push
+	mov	edx, 0
+	mov	esi, 3
+	mov	edi, 0
+	call	push
+	mov	edx, -1
+	mov	esi, 2
+	mov	edi, 0
+	call	push
+	mov	edx, 0
+	mov	esi, 1
+	mov	edi, 0
+	call	push
+	mov	edx, 0
+	mov	esi, 12
+	mov	edi, 0
+	call	push
+	mov	edx, 1
+	mov	esi, 0
+	mov	edi, 0
+	call	push
+	mov	edx, -1
+	mov	esi, 2
+	mov	edi, 0
+	call	push
+	nop
+.L24:
+	mov	edi, 0
+	call	size
+	test	eax, eax
+	jne	.L25
+	mov	eax, 1
+	jmp	.L63
+.L25:
+	mov	edi, 0
+	call	pop
+	mov	QWORD PTR -32[rbp], rax
+	mov	eax, DWORD PTR -28[rbp]
+	test	eax, eax
+	jne	.L27
+	mov	eax, DWORD PTR -32[rbp]
+	cmp	eax, 12
+	ja	.L28
+	mov	eax, eax
+	lea	rdx, 0[0+rax*4]
+	lea	rax, .L30[rip]
+	mov	eax, DWORD PTR [rdx+rax]
+	cdqe
+	lea	rdx, .L30[rip]
+	add	rax, rdx
+	jmp	rax
+	.section	.rodata
+	.align 4
+	.align 4
+.L30:
+	.long	.L42-.L30
+	.long	.L66-.L30
+	.long	.L67-.L30
+	.long	.L68-.L30
+	.long	.L69-.L30
+	.long	.L70-.L30
+	.long	.L71-.L30
+	.long	.L72-.L30
+	.long	.L73-.L30
+	.long	.L74-.L30
+	.long	.L75-.L30
+	.long	.L76-.L30
+	.long	.L65-.L30
+	.text
+.L42:
+	mov	eax, 0
+	jmp	.L63
+.L28:
+	mov	eax, 1
+	jmp	.L63
+.L27:
+	mov	edx, DWORD PTR -28[rbp]
+	mov	eax, DWORD PTR -32[rbp]
+	mov	esi, eax
+	mov	edi, 1
+	call	push
+	jmp	.L24
+.L66:
+	nop
+.L43:
+	mov	edi, 1
+	call	peek
+	sar	rax, 32
+	mov	r12d, eax
+	mov	edi, 1
+	call	pop
+	mov	ebx, eax
+	mov	edi, 1
+	call	pop
+	mov	edx, r12d
+	mov	esi, ebx
+	mov	edi, eax
+	call	push
+	jmp	.L24
+.L67:
+	nop
+.L44:
+	mov	edi, 1
+	call	peek
+	mov	edi, eax
+	call	peek
+	sar	rax, 32
+	mov	ebx, eax
+	mov	edi, 1
+	call	pop
+	mov	edi, eax
+	call	pop
+	mov	edx, ebx
+	mov	esi, eax
+	mov	edi, 1
+	call	push
+	jmp	.L24
+.L68:
+	nop
+.L45:
+	mov	edi, 1
+	call	peek
+	mov	edi, eax
+	call	peek
+	sar	rax, 32
+	mov	ebx, eax
+	mov	edi, 1
+	call	pop
+	mov	edi, eax
+	call	peek
+	mov	edx, ebx
+	mov	esi, eax
+	mov	edi, 1
+	call	push
+	jmp	.L24
+.L69:
+	nop
+.L46:
+	mov	edi, 1
+	call	pop
+	mov	ebx, eax
+	mov	edi, 1
+	call	pop
+	mov	esi, ebx
+	mov	edi, eax
+	call	ren
+	jmp	.L24
+.L70:
+	nop
+.L47:
+	mov	edi, 1
+	call	pop
+	mov	ebx, eax
+	mov	edi, 1
+	call	pop
+	mov	esi, ebx
+	mov	edi, eax
+	call	clone
+	jmp	.L24
+.L71:
+	nop
+.L48:
+	mov	edi, 1
+	call	pop
+	mov	edi, eax
+	call	flush
+	jmp	.L24
+.L72:
+	nop
+.L49:
+	mov	edi, 1
+	call	peek
+	sar	rax, 32
+	mov	ebx, eax
+	mov	edi, 1
+	call	pop
+	mov	r12d, eax
+	mov	edi, 1
+	call	pop
+	add	eax, r12d
+	mov	edx, ebx
+	mov	esi, eax
+	mov	edi, 1
+	call	push
+	jmp	.L24
+.L73:
+	nop
+.L50:
+	mov	edi, 1
+	call	peek
+	sar	rax, 32
+	mov	ebx, eax
+	mov	edi, 1
+	call	pop
+	mov	r12d, eax
+	mov	edi, 1
+	call	pop
+	mov	edx, eax
+	mov	eax, r12d
+	sub	eax, edx
+	mov	edx, ebx
+	mov	esi, eax
+	mov	edi, 1
+	call	push
+	jmp	.L24
+.L74:
+	nop
+.L51:
+	mov	edi, 1
+	call	peek
+	sar	rax, 32
+	mov	ebx, eax
+	mov	edi, 1
+	call	pop
+	mov	r12d, eax
+	mov	edi, 1
+	call	pop
+	imul	eax, r12d
+	mov	edx, ebx
+	mov	esi, eax
+	mov	edi, 1
+	call	push
+	jmp	.L24
+.L75:
+	nop
+.L52:
+	mov	edi, 1
+	call	peek
+	sar	rax, 32
+	mov	ebx, eax
+	mov	edi, 1
+	call	pop
+	pxor	xmm2, xmm2
+	cvtsi2ss	xmm2, eax
+	movss	DWORD PTR -52[rbp], xmm2
+	mov	edi, 1
+	call	pop
+	pxor	xmm0, xmm0
+	cvtsi2ss	xmm0, eax
+	movss	xmm1, DWORD PTR -52[rbp]
+	divss	xmm1, xmm0
+	movss	xmm0, DWORD PTR .LC1[rip]
+	divss	xmm0, xmm1
+	cvttss2si	eax, xmm0
+	mov	edx, ebx
+	mov	esi, eax
+	mov	edi, 1
+	call	push
+	jmp	.L24
+.L76:
+	nop
+.L53:
+	mov	edi, 1
+	call	peek
+	sar	rax, 32
+	cmp	eax, 3
+	je	.L55
+	cmp	eax, 3
+	jg	.L56
+	cmp	eax, 1
+	je	.L57
+	cmp	eax, 2
+	je	.L58
+	jmp	.L56
+.L57:
+	mov	edi, 1
+	call	pop
+	mov	esi, eax
+	lea	rax, .LC2[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	jmp	.L59
+.L58:
+	mov	edi, 1
+	call	pop
+	mov	esi, eax
+	lea	rax, .LC3[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	jmp	.L59
+.L55:
+	mov	edi, 1
+	call	pop
+	mov	edi, eax
+	call	putchar@PLT
+	jmp	.L59
+.L56:
+	mov	eax, 2
+	jmp	.L63
+.L59:
+	jmp	.L24
+.L65:
+	nop
+.L54:
+	mov	edi, 1
+	call	pop
+	sar	rax, 32
+	cmp	eax, 1
+	je	.L60
+	cmp	eax, 3
+	je	.L61
+	jmp	.L62
+.L60:
+	lea	rax, -32[rbp]
+	mov	rsi, rax
+	lea	rax, .LC2[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	__isoc99_scanf@PLT
+	mov	DWORD PTR -28[rbp], 1
+	jmp	.L62
+.L61:
+	mov	rax, QWORD PTR stdin[rip]
+	mov	rdi, rax
+	call	fgetc@PLT
+	mov	DWORD PTR -32[rbp], eax
+	mov	DWORD PTR -28[rbp], 3
+	nop
+.L62:
+	mov	edx, DWORD PTR -28[rbp]
+	mov	eax, DWORD PTR -32[rbp]
+	mov	esi, eax
+	mov	edi, 1
+	call	push
+	jmp	.L24
+.L63:
+	mov	rdx, QWORD PTR -24[rbp]
+	sub	rdx, QWORD PTR fs:40
+	je	.L64
+	call	__stack_chk_fail@PLT
+.L64:
+	add	rsp, 48
+	pop	rbx
+	pop	r12
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE13:
 	.size	main, .-main
+	.section	.rodata
+	.align 4
+.LC1:
+	.long	1065353216
 	.ident	"GCC: (GNU) 14.2.1 20240910"
 	.section	.note.GNU-stack,"",@progbits
